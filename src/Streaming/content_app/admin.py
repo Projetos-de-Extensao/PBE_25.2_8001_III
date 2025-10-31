91f16e6b727d31edc7193605b1d3c09ac1627ed3
Pietro <youremail@yourdomain.com>
2025-10-31 11:00:25 -0300

    aula 31/10

--- src/Streaming/content_app/admin.py
+++ src/Streaming/content_app/admin.py
@@ -4,5 +4,12 @@ from content_app import models
 
 
 # Register your models here.
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content_type','is_public')
+    search_fields = ('title', 'description')
+    list_filter = ('content_type', 'is_public', 'is_public')
+    ordering = ('-upload_date',)
 
-admin.site.register(models.Content)
+
+admin.site.register(models.Content, ContentAdmin)
+admin.site.register(models.Playlist)
